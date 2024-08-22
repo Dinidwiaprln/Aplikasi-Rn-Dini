d39c239848af9ee8dad1e7a2b019be085508f4c4
Dinidwiaprln <dinidwiaprln24@gmail.com>
2024-08-22 16:00:40 +0700

    perubahan pertama

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View} from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text> Hello Wordl </Text>
+      <Text> XI RPL 2 </Text>
+    </View>
+  )
+}
+export default App
